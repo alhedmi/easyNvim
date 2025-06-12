5afec05ce457118f10f5b6a328411ff8fc4bf5d1
Amir <amiralhedmi@gmail.com>
2025-06-12 22:03:59 +0200

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,21 @@
+-- ███╗   ██╗██╗   ██╗██╗███╗   ███╗
+-- ████╗  ██║██║   ██║██║████╗ ████║
+-- ██╔██╗ ██║██║   ██║██║██╔████╔██║
+-- ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║
+-- ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║
+-- ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+-- Amir's NeoVim Config (modular & portable)
+
+require("amir.config")
+
+require("amir.settings")
+
+require("amir.keymaps")
+
+require("amir.plugins")
+
+require("amir.ui")
+
+require("amir.lsp")
+
+require("amir.extras")
