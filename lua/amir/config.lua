add216179d9c677d8a1a45eae6b5540a9673aab8
Amir <amiralhedmi@gmail.com>
2025-06-12 22:07:55 +0200

    Create config.lua

---
+++ lua/amir/config.lua
@@ -0,0 +1,18 @@
+-- lua/amir/config.lua
+-- Global config flags for easyNvim
+
+local M = {}
+
+M.theme = "tokyonight"   -- Options: "tokyonight", "gruvbox", "catppuccin"
+M.use_fancy_ui = true     -- Enable lspsaga, noice.nvim, etc.
+
+-- Extras (toggle plugins)
+M.enable_terminal = true
+M.enable_dashboard = false
+M.enable_surround = true
+M.enable_autopairs = true
+M.enable_signature_help = true
+M.enable_lspsaga = false
+M.enable_indent_guides = true
+
+return M
